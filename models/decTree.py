4cb7dbf0ad9975926048d46c3b3eb2a49bedcab5
Daniel Le <ledanieln@gmail.com>
2019-04-28 22:24:08 -0500

    update decTree.py with labelEncoder

--- models/decTree.py
+++ models/decTree.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn import tree
+from sklearn import preprocessing
 import graphviz
 import os
 import numpy as np
@@ -11,20 +12,32 @@ os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 #load data
 data = pd.read_csv('../data/processed/train.csv')
 
+#preprocess data, change string data to numerical
+Yle = preprocessing.LabelEncoder()
+Yle.fit(list(data['zoneType'].unique()))
+newY = Yle.transform(Yle.classes_)
+print(newY)
+
+oldY = list(Yle.inverse_transform(newY))
+
+print(oldY)
+
 #get relevant data for feature training
 dataNoID = data.drop(['zoneID', 'zoneType'], axis=1)
-X = pd.concat( [data['zoneArea'], data['StructureCount']], axis=1).values
+X = data.values
+
+
 
 #get target data
-Y = data['zoneType'].values
+Y = data['zoneType']
 
 print(type(X))
 print(type(Y))
 
 #build decision tree
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-dot_data = tree.export_graphviz(clf, out_file=None)
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(X, Y)
+# dot_data = tree.export_graphviz(clf, out_file=None)
 
-graph = graphviz.Source(dot_data)
-graph.render('test')
+# graph = graphviz.Source(dot_data)
+# graph.render('test')
